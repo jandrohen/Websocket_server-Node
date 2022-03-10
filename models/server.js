30476eb35e5c14a4ac776c9e9ae2fef3ef9c414a
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-03-10 12:48:21 +0100

    (lesson 203 course node) install library socket.io and first test as a clients of the service

--- models/server.js
+++ models/server.js
@@ -6,6 +6,8 @@ class Server{
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer( this.app );
+        this.io = require('socket.io')( this.server );
 
         this.paths = {}
 
@@ -29,7 +31,7 @@ class Server{
     routes() {}
 
     listen() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log('Server listening on port', this.port);
         });
     }
