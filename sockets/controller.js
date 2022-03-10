a97915c1c7b6ef8e5a9e0cd21188a356d12dfe6e
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-03-10 19:51:22 +0100

    (lesson 210-214 course node) Add design interfaz statics * Start of the project for a ticketing application to manage a waiting room with tickets

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,10 +1,4 @@
 const socketController = (socket) => {
-    console.log('Client connected', socket.id);
-
-    socket.on('disconnect', () =>{
-        console.log('client disconnected', socket.id);
-    })
-
     socket.on('send-msg', (payload, callback) => {
         const id = 123456;
         callback( id );
