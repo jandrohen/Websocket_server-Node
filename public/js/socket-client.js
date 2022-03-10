71127552559d9087b83f3ebbf1f6a7dfd68261db
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-03-10 13:14:33 +0100

    (lesson 205 course node) Connection and disconnection messages on the client

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,19 @@
 
+// Reference HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', ()=>{
+    console.log('connect');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+})
+
+socket.on('disconnect', ()=>{
+    console.log('disconnect');
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+})
