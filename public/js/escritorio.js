efe56a280a3ba092e8f1b7e98d6dcd0f5869fc7e
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-03-11 14:24:53 +0100

    (lesson 218 course node) Prepare desktop screen * Add references from escritorio.html and listen emit socket server

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,33 @@
+// Reference HTML
+const lbldesktop = document.querySelector('h1');
+const btnNew = document.querySelector('button');
 
+const searchParams = new URLSearchParams( window.location.search );
 
-console.log('Escritorio HTML');
+if ( !searchParams.has('escritorio')){
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const desktop = searchParams.get('escritorio');
+lbldesktop.innerText = desktop;
+
+const socket = io();
+
+socket.on('connect', ()=>{
+    btnNew.disabled = false;
+})
+
+socket.on('disconnect', ()=>{
+    btnNew.disabled = true;
+})
+
+socket.on('last-ticket', (lastTicket) =>{
+    // lblNewTicket.innerText = 'Ticket ' + lastTicket;
+})
+
+btnNew.addEventListener('click', ()=>{
+    // socket.emit( 'next-ticket', null, ( ticket )=>{
+    //     lblNewTicket.innerText = ticket;
+    // });
+});
